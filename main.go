c5f2c901c3c7b2836c103518d556f421c7c0e883
jiayuxin <13520723393@163.com>
2022-04-12 17:54:02 +0800

    交替打印实现方法错误

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func main() {
-	println("Hello, world.")
+	fmt.Println("Hello, world.")
+}
+
+// InterleavePrintOneChan
+// 1.交替打印数字和字母
+// 12AB34CD56EF78GH910IJ1112KL1314MN1516OP1718QR1920ST2122UV2324WX2526YZ2728
+func InterleavePrintOneChan() {
+	notify := make(chan struct{})
+	var wg sync.WaitGroup
+	wg.Add(2)
+	printNums := func() {
+		defer wg.Done()
+		for i := 1; i < 10000; {
+			fmt.Print(i)
+			fmt.Print(i + 1)
+
+			i += 2
+			if _, ok := <-notify; !ok {
+				break
+			}
+		}
+	}
+	printAB := func() {
+		defer wg.Done()
+		defer close(notify)
+		for i := 'A'; i < 'Z'; {
+			notify <- struct{}{}
+			fmt.Print(string(i))
+			fmt.Print(string(i + 1))
+			fmt.Print()
+			i += 2
+		}
+	}
+	go printNums()
+	go printAB()
+	wg.Wait()
 }
--- main_test.go
+++ main_test.go
@@ -1,9 +1,10 @@
-package main_test
+package main
 
 import (
 	"testing"
 )
 
-func TestDemo(t *testing.T) {
-	t.Logf("hello world")
+// Test InterleavePrintOneChan
+func TestInterleavePrint(t *testing.T) {
+	InterleavePrintOneChan()
 }
